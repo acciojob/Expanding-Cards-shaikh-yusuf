8253e5f5b18fb18a13aacfbcacedbf2e0bd95b43
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-11-22 19:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function expand(panelNumber) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => panel.classList.remove('active'));
+    panels[panelNumber - 1].classList.add('active');
+  }
